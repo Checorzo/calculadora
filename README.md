7bf1487f853360c9d488de3d43cf8ba0034f6c24
Sergio Corzo <shndzcorzo@gmail.com>
2023-02-09 12:47:44 -0600

    Listas funcionalidades basicas de la calculadora

--- README.md
+++ README.md
@@ -1,9 +1,53 @@
-# Calculadora
-App para hacer operaciones matematicas con botones para los números y operadores matemáticos.
+# Calculadora --- App para hacer operaciones matematicas con botones para los números y operadores matemáticos. La aplicación debe soportar numero de reales, de comoa flotante. Cuando un usuario ya haya puesto un punto al numero la aplicacion debera bloquear el boton del punto así como no permitir poner más de 1 punto por número, ya sean 2 punto juntos o alternados con digitos del mismo número. También debe calcalar las operaciones de manera automatica conforme el usuario vaya introduciendo los numeros y operadores. Debe de visualizarse el resultado con un color claro, es decir, sin que el usuario no haya presionado el botón de igual. Cuando se el botón de igual sea presionado el color del resultado de cambiar a un color mas oscuro, asi como  guardar tanto la operacion como el resultado en un historial. Debe ser capaz de realizar operacines con el valor del resulta previo obtenido si es que el
+primero valor que sea digitado por el usuario es un operador, de lo contrario hara una
+operacion normal.
+La aplición debera tener 2 cajas para visualizar la operación y el otro el resultdo. Mientras 
+no se haya presionado el botnn de resultado la operación se visualizara en una caja y el 
+resultado en otra. Despues de preionado el igual el resultado se viaulozará en la caja donde
+se visulizaba la operación. Si despues se oprime un número o punto se limpiara pantalla y se 
+imprimir el nuevo numero. De lo contrario se usara el resultado como parte de la nueva operación
 
-## Pasos para crear la App
-- Crear 2 cajas de textos para previsualizar y visualiar resultado y operadores.
+## Que se espera que la aplicción haga
+   - Hacer operaciones automaticas y motrar el resultado conforme se vayan introduciendo 
+   - los valores sin necesidad de presionar el boton igual.
+   - Validar sin un valor numerico es valido, que no tenga mas de un punto.
+   - visualizar tanto operación como resultado simultaneamente.
+   - Despues de precionado el igual limpiar pantallaen ambas cajas de texto, despues 
+     imprimir el resultado en la caja donde se visualizaba la operaciónn.
+   - Guardar las operaciones y resultado como historial.
+   - operar con el resultado previo si el primer valor es un operador.
+
+## Variables
+   - Variable operandoA, operandoB y resultado y  de tipo **Real** 
+   - Variable historial para guadar el historial de de operaciones y resultado.
+   - 1 variable para abstraerlo los botones de los números.
+   - Variables controladoras para permitir o bloquear ciertas funciones.
+
+
+## Datos disponibles
+- Se requieren botones de 0 al nueve, los operadores de suma, resta, multiplicación y división,
+  el botón del punto, un botón para el historial, un botón para borrar y uno para limpiar
+  pantalla
+- Un elemento para visualizar la operacion y uno para el resultado.
+- El boton de igual debe de imprimir el resultado final.
+  
+
+## Pasos para crear la App 
+- Hacer la estructura en html.
+- Darles los estilos con css para que se vea como la calculadora de android.
+
 - Crear los botones de los numeros del 0-9 y los operadores de suma, resta, multiplicacion, division, porcentaje, parentesis, borrrar, punto flotante, más_menos, limpiar pantalla e igual.
-      ![Calculadora  imagen](./src/calculadora.jpg)
+
+|     ![Calculadora  imagen](./src/calculadora.jpg) |
+|---------------------------------------------------|
+| <b>La imagen es solo representativa, una guía.</b> |
+
+## Crear la apicacoin en JavaScript
+
+```
+Algoritmo Calculadora 
+   
+FinAlgoritmo
+```
 
 
---
+++ js/calculadora.js
@@ -0,0 +1,192 @@
+   let lastValue =  null;
+   let newValue = null;
+   let result = null;
+   let temp = '';
+   let operators = /-/;
+   let operator = '';
+   let digit = '';
+   let lastDigit = '';
+   let stringNumber = '';
+     
+   /*Variables centinelas o banderas*/
+   let dotFlag = false;
+   let operatorFlag = false;
+   let numberFlag = false;
+   let minusFlag = true;
+   let operationStatue = false;
+   let newNumberStatus = false;
+   let operationFlag = false;
+   let operationAcumalte = false;
+   let calculateStatus = false;
+   let counterOperator = 0;
+
+   /*Variables para avstraer los botones*/
+   const screenOperation = document.querySelector('#operation');
+   const screenResult = document.querySelector("#result");
+   
+   
+   const key = document.querySelector('.keyboard-number');
+    //----------------------------------------------------------
+   //Capturar botones e imprimir 
+   const calcular = ()=>{
+  //Agregar listener a la  calculadora 
+      key.addEventListener('click', e=>{
+         const t = e.target, 
+            d = t.name
+
+         if(d === 'number'){
+            digit = t.textContent;
+            if( operationFlag === true ){
+               screenOperation.value = '';
+               operationFlag = false;
+               newValue = 0;
+               console.log("operationFlag");
+            }
+            numberFlag = true;
+            operatorFlag = true;
+            printValues(screenOperation,digit);
+           if(operationStatue === true){
+               newValue = parseFloat(screenOperation.value);
+               operationStatue = true;
+            }
+            else{
+               counterOperator = 0;
+            }
+         }
+         if(d === 'operator' && numberFlag === true && operatorFlag === true){
+            operator = t.textContent;
+            newNumberStatus = true;
+            operationFlag = true;
+            counterOperator++;
+            operationStatue = true;
+
+            if(operationStatue === true ){
+               operationAcumalte = true;
+               newNumberStatus = true;
+               console.log(lastValue);
+               if( counterOperator > 1 ){
+               lastValue = result;
+               operationStatue = true;
+               counterOperator = 1;
+               }
+               else{
+               lastValue = parseFloat(screenOperation.value);
+               }
+            }
+           
+
+
+            screenOperation.value = screenOperation.value + operator;
+
+            console.log(lastValue,operator);
+            if( operator)minusFlag = false;
+            if(operator === 'x' || operator === '÷')minusFlag = true;
+            dotFlag = false;
+            operatorFlag = false;
+            
+         }
+         if(d === 'operation'){
+            if(t.textContent === 'C'){
+               clear();
+            }
+
+            if(t.classList[0] === "erase"){
+               erase();
+               if(operationStatue === false)lastValue = parseFloat(screenOperation.value);
+               else{
+                  newValue = parseFloat(screenOperation.value);
+            }
+         }
+
+         if(t.textContent === '='){
+               console.log(lastValue, newValue)
+               calculate(operator);
+               printResult(result);
+               screenOperation.value = '';
+               operationFlag = true;
+               operationStatue = false;
+            }
+         }
+         if(d ==='dot' && dotFlag === false){
+         //   console.log(`dot: ${t.textContent}`);
+            printValues(screenOperation,t.textContent);
+            dotFlag = true;
+         }
+      }//fin del listener de la calculadora
+   )}//fin de captura de botones
+   /*--------------------------------------------------------*/
+
+   function printValues( out, value ){
+      out.value = out.value + value;      
+   }
+   function printResult(out){
+      screenResult.value = out;
+      // screenOperation.value = '';
+   }
+
+
+   function clear(){
+
+      screenOperation.value = '';
+      screenResult.value = '';
+      lastValue = 0;
+      firstNumber = true;
+      newValue = 0;
+      result = 0;
+      lastDigit = '';
+      operatorFlag = false;
+      operationStatue = false;
+      stringNumber = '';
+      operationAcumalte = true;
+
+        }
+
+   function erase(){
+      screenOperation.value = screenOperation.value.slice( 0,-1 );
+      //console.log('borrar digito');
+   }
+   function plus(operandA, operandB){
+      return operandA + operandB;
+   }
+   function minus( operandA, operandB ){
+      return operandA - operandB;
+   }
+   function multiply( operandA, operandB ){
+      return operandA * operandB;
+   }
+   function divition( operandA, operandB ){
+      return operandA / operandB;
+   }
+
+
+   function calculate(value){
+      calculateStatus = true;
+      switch(value){
+         
+         case '+': 
+            lastValue = plus(lastValue, newValue);
+            result = lastValue;
+            console.log(lastValue,newValue,result);
+            break;
+         
+         case '-': 
+            result = minus(lastValue, newValue);
+            break;
+         
+         case 'x': 
+            lastValue = multiply(lastValue, newValue);
+            result = lastValue;
+            break;
+         
+         case '÷': 
+            lastValue = divition(lastValue, newValue);
+            result = lastValue;
+            break;
+
+         default:
+            calculateStatus === false;
+            console.log("No se encontrado operación ", );
+            break;
+      }
+   }//fin de funcion calcular
+   calcular();
